8ef8ebbb1287b8d93423f1d8c3a09e16936a970c
zero <gaa40@lri.fr>
2019-10-22 16:31:53 +0200

    cleaned app2 script

--- app2.py
+++ app2.py
@@ -5,9 +5,7 @@ import time
 import datetime
 import werkzeug
 import sys
-import urllib
 import traceback
-from json import dumps
 import cv2
 import imutils
 
@@ -26,7 +24,7 @@ import darknet as dn
 UPLOAD_FOLDER = os.path.join(base_path,'uploads')
 
 ALLOWED_IMAGE_EXTENSIONS = set(['png','jpg','jpeg'])
-ALLOWED_VIDEO_EXTENSIONS = set(['avi','mp4'])
+ALLOWED_VIDEO_EXTENSIONS = set(['avi','mp4','mov'])
 ALLOWED_DETECTIONS = set(['tag','product'])
 
 app = flask.Flask(__name__)
@@ -166,28 +164,11 @@ class Detector():
     def __init__(self):
         #define weights, data, cfg path and load net, paths should be in utf-8
         base_path = os.getcwd()
-        #data_path = 'darknet/data/globus_13.data'
         data_path2 = 'darknet/data/globus_tags.data'
-        #cfg_path = 'darknet/cfg/globus13-yolov3.cfg'
         cfg_path2 = 'darknet/cfg/globustags-yolov3.cfg'
-        #weights_path = 'darknet/backup/globus13-yolov3_2000.weights'
-        weights_path2 = 'darknet/backup/globustags-yolov3_1000.weights'
+        weights_path2 = 'darknet/backup/globustags-yolov3.weights'
 
-        #read gtins and save them to a data structure
-        reader =csv.DictReader(open('globus_13_gtins.csv','r'))
-        dict_l = []
-        self.dict_g = {}
-        for line in reader:
-            dict_l.append(line) 
-
-        for coi in dict_l:
-            self.dict_g[coi['class']] = coi['gtin']
-
-
-        
-        #load network and meta using darkflow library
-        #self.net = dn.load_net(os.path.join(base_path,cfg_path).encode('utf-8'),os.path.join(base_path,weights_path).encode('utf-8'),0)
-        #self.meta = dn.load_meta(os.path.join(base_path,data_path).encode('utf-8'))
+       #TODO: add barcode and digit extraction
 
         self.net2 = dn.load_net(os.path.join(base_path,cfg_path2).encode('utf-8'),os.path.join(base_path,weights_path2).encode('utf-8'),0)
         self.meta2 = dn.load_meta(os.path.join(base_path,data_path2).encode('utf-8'))
@@ -198,29 +179,23 @@ class Detector():
         net1 = False
         net2 = False
         
-        #if self.net:
-        #    net1= True
-
         if self.net2:
             net2 = True
 
         return dict(net1_state=net1,net2_state=net2)
 
-    # detect image for groceries or tags
+#detect image
     def detect_image(self, filename,to_detect):
         try:
             s= time.time()
-
-            #if to_detect == 'product':
-            #    res = dn.detect(self.net,self.meta,filename.encode('utf-8'))
-            #else:
-            #    return(False,[],0)
-            
+            img = cv2.imread(filename)
+            print(img.shape)
             if to_detect == 'tag':
-                res = dn.detect(self.net2,self.meta2,filename.encode('utf-8'))
+                res = dn.detect(self.net,self.meta,filename.encode('utf-8'))
+                #res = dn.detect_image(self.net,self.meta,img) # set threshold
             else:
                 return(False,[],0)
-
+        
             e = time.time() - s
             if len(res) > 0:
                 # process the output into a dictionary
@@ -228,141 +203,94 @@ class Detector():
                 lis = []
                 while(i < len(res)):
                     class_type = res[i][0].decode('utf-8')
-            
-                    center_x = int(res[i][2][0])
-                    center_y = int(res[i][2][1])
-                    width = int(res[i][2][2])
-                    height = int(res[i][2][3])
+                    pt1,pt2  = cvDrawBoxes(res[i])
+                    y_mn = max(pt1[1],0)
+                    x_mn = max(pt1[0],0)
 
-                    UL_x = int(center_x - width/2) #Upper Left corner X coord
-                    #sometimes we get negative coords when the box is outside the image or frame
-                    if UL_x < 0:
-                        UL_x = 0
-                    UL_y = int(center_y + height/2) #Upper left Y
-                    LR_x = int(center_x + width/2)
-                    LR_y = int(center_y - height/2)
-                    if LR_y < 0:
-                        LR_y = 0
-                    #old return
-                    #lis.append(dict(cls=class_type,x=UL_x,y=UL_y,w=width,h=height))
-                    #new return (dummy data gtin and price)
-                    #the new return contains dummy data for the sake of project completion, this section will be updated when the new model is uploaded
-                    lis.append(dict(typ='Tag',gtin='7613033635205',txt='4.79',x=UL_x,y=UL_y,w=width,h=height))
+                    w = pt2[0] - x_mn
+                    h = pt2[1] - y_mn
+        
+                    # debug
+                    #cv2.rectangle(img, pt1, pt2, (0, 255, 0), 3)
+                    #cv2.putText(img,class_type +" [" + str(round(res[i][1] * 100, 2)) + "]",(pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,[0, 255, 0], 2)
+                    try:
+                        lis.append(dict(typ='Tag',gtin="dummy data",txt='',x=x_mn,y=y_mn,w=w,h=h))
+                    except Exception as e2:
+                        logging.error(e2)
                     i+=1
-                # draw results for debugging
-                im  = cvDrawBoxes(res,cv2.imread(filename))
-                im = cv2.cvtColor(im,cv2.COLOR_RGB2BGR)
-                cv2.imwrite('result.png',im)
-                
+
                 rtn = (True,lis,e)
             else:
                 rtn = (False,[],0)
             return rtn
-        except:
+        except Exception as e:
             logging.info('Error in detect_image method\n')
-            traceback.print_exc()
+            logging.error(str(e))
 
-    
+
+#detect_video
     def detect_video(self,filename,to_detect):
-        try:
-            # get network size
-            # change line below to self.net2 if tags to be detected
-            
-            #net_w = dn.network_width(self.net)
-            #net_h = dn.network_height(self.net)
-
-            net_w = dn.network_width(self.net2)
-            net_h = dn.network_height(self.net2)
-
-            # read video and set size 
+        try:        
+            if not to_detect == 'tag':
+                return (False,[],0)
+           
             cap = cv2.VideoCapture(filename)
-            cap.set(3,1280)
-            cap.set(4,720)
-
+           
+            # if the video is rotated, we need to read that flag
+            #cap.set(3,1280) with opposite value
+            #cap.set(4,720)
+           
             # get frames count from file
             prop = cv2.cv.CV_CAP_PROP_FRAME_COUNT if imutils.is_cv2() else cv2.CAP_PROP_FRAME_COUNT
+            
             total_frames = int(cap.get(prop))
             # log info
             logging.info('File uploaded : {} , total number of frames in video {}'.format(filename,total_frames))
-
-            #create a darknet image to be used for each extracted frame
-            darknet_image = dn.make_image(net_w,net_h,3)
-
-            # create a new video to write images and save them
-            out = cv2.VideoWriter(os.path.join(os.getcwd(),'output.avi'),cv2.VideoWriter_fourcc(*"MJPG"),30.0,(net_w,net_h))
             
-            # list of results for frames
             lis_outer = []
-            #call forward when start grabing the frames
             grab, frame = cap.read()
+            
+            f_h,f_w,f_c = frame.shape
+
+            darknet_image = dn.make_image(f_w,f_h,f_c)
+               
+            #out = cv2.VideoWriter(os.path.join(os.getcwd(),'output-{}.avi'.format(filename)),cv2.VideoWriter_fourcc(*"MJPG"),30.0,(f_w,f_h))
+            
             s = time.time()
             counter_out = 1
-
+            
             while grab:
-                frame_rgb = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-                frame_resized = cv2.resize(frame_rgb,(net_w,net_h),interpolation=cv2.INTER_LINEAR)
-                dn.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
-
-                # decide which model to use for detection based on input
-
-                # time per frame uncomment below
-                # inn_t = time.time()
-
-                #if to_detect == 'product':
-                #    res = dn.detect_image(self.net,self.meta,darknet_image) # assign a threshold
-                #else:
-                #    return (False,[],0)
-
-                if to_detect == 'tag':
-                    res = dn.detect_image(self.net2,self.meta2,darknet_image) # assign a threshold
-                else:
-                    return (False,[],0)
-
-                # time per frame uncomment below
-                #inn_t = time.time() - inn_t
-
-                # save results to a dict and add it to a list
+                dn.copy_image_from_bytes(darknet_image,frame.tobytes())
+                res = dn.detect_image(self.net,self.meta,darknet_image,0.65)
                 lis_inner = []
                 inner = 0
-
+                
                 while(inner < len(res)):
                     class_type = res[inner][0].decode('utf-8')
-            
-                    center_x = int(res[inner][2][0])
-                    center_y = int(res[inner][2][1])
-                    width = int(res[inner][2][2])
-                    height = int(res[inner][2][3])
-
-                    UL_x = int(center_x - width/2) #Upper Left corner X coord
-                    if UL_x < 0:
-                        UL_x = 0
-                    UL_y = int(center_y + height/2) #Upper left Y
-                    LR_x = int(center_x + width/2)
-                    LR_y = int(center_y - height/2)
-                    if LR_y < 0:
-                        LR_y = 0
-                    #old return
-                    #lis_inner.append(dict(cls=class_type,x=UL_x,y=UL_y,w=width,h=height))
-                    #new return (dummy data gtin and price)
-                    #the new return contains dummy data for the sake of project completion, this section will be updated when the new model is uploaded
-                    lis.append(dict(typ='Tag',gtin='7613033635205',txt='4.79',x=UL_x,y=UL_y,w=width,h=height))
-                    inner+=1 
-
+                    pt1,pt2  = cvDrawBoxes(res[inner])
+                    y_mn = max(pt1[1],0)
+                    x_mn = max(pt1[0],0)
+                    
+                    w = pt2[0] - x_mn
+                    h = pt2[1] - y_mn
+                    
+                    #debug
+                    #cv2.rectangle(frame, pt1, pt2, (0, 255, 0), 3)
+                    #cv2.putText(frame,class_type +" [" + str(round(res[inner][1] * 100, 2)) + "]",(pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,[0, 255, 0], 2)
+                   
+                    lis_inner.append(dict(typ='Tag',gtin='dummy number',txt='',x=x_mn,y=y_mn,w=w,h=h))
+                    inner+=1
                 lis_outer.append(dict(frame=counter_out,objects=lis_inner))
+            
                 if inner > 0:
                     counter_out+=1
-                
-                # for debuging draw the detections around the frame and save the file to check it later
-                im  = cvDrawBoxes(res,frame_resized)
-                im = cv2.cvtColor(im,cv2.COLOR_RGB2BGR)
-
-                out.write(im)
-                
+            
+                #out.write(frame)
                 grab, frame = cap.read()
     
             t = time.time() - s
             cap.release()
-            out.release()
+            #out.release()
 
             if counter_out > 0:
                 rtn = (True,lis_outer,t)
@@ -370,8 +298,9 @@ class Detector():
                 rtn = (False,[],0)
             return rtn
         
-        except:
+        except Exception as e:
             logging.info('Error in detect video method')
+            logging.error(str(e))
             traceback.print_exc()
 
     
